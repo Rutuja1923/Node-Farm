bc7bf672a8c62760cdfdef02b1313790fd116c81
Rutuja1923 <rutujajirage19@gmail.com>
2025-01-19 21:40:58 +0530

    Read-wrie into file

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const fs = require('fs');
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 
 console.log(textIn);
+
+const textOut = `This is what we what we know about the avocadoL ${textIn}.\nCreateed on ${Date.now()}` ;
+
+fs.writeFileSync('./txt/output.txt',textOut);
+
+console.log('File written!');
+
